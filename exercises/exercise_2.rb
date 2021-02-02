8d095f489e103bf16217bcd8b3af3aa8ade59458
alisonhussey <alisonhussey@gmail.com>
2021-02-02 21:00:51 +0000

    gave two stores instance variables, changes store1's name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "Burnbabyburn"
+# puts @store1.name
+@store1.save!
+
+p Store.all.map(&:name)
+
+# p @store1
